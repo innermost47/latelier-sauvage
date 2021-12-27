5c030363a55d5b153cbb40b7a37f227442826b61
anthony <anthony.charretier@gmail.com>
2021-12-27 15:58:53 +0100

    add welcome section

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,24 @@
+import "../style/welcome.css";
+import { welcomeDatas } from "../datas/welcomeDatas";
+
 const Welcome = () => {
-  return <div></div>;
+  return (
+    <section id="welcome">
+      <div className="container">
+        <div className="row">
+          <h2 className="sectionTitle">VOUS SOUHAITEZ</h2>
+          {welcomeDatas.map((data, key) => {
+            return (
+              <div key={key} className="col-md-4">
+                <img src={data.image} alt="image" className="icon" />
+                <p>{data.content}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
