e3872640092d432c1fedb5540f7988d797367010
anthony <anthony.charretier@gmail.com>
2022-01-02 19:10:46 +0100

    fixed margin bottom on who ami i update

--- src/components/UpdateWhoAmI.js
+++ src/components/UpdateWhoAmI.js
@@ -57,7 +57,7 @@ const UpdateWhoAmI = () => {
       <h2>Modifier Qui suis-je</h2>
       <div className="mb-4 mt-4">
         <div
-          className="mb-4"
+          className="mb-3"
           dangerouslySetInnerHTML={{ __html: decode(content) }}
         ></div>
         <form onSubmit={handleSubmit(onSubmit)}>
--- src/js/Constantes.js
+++ src/js/Constantes.js
@@ -1,3 +1,3 @@
-//export const url = "https://api.latelier-sauvage.com/";
-export const url = "http://localhost:8000/";
+export const url = "https://api.latelier-sauvage.com/";
+//export const url = "http://localhost:8000/";
 export const recaptchaKey = "6LdrgHgaAAAAALSBQ7cG71q1lpeHoTg73vQYR4cY";
